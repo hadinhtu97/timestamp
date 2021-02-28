1b3b5adc3c18fb764fa9c6c487c02289935d9489
Tu Ha Dinh <hadinhtu97@gmail.com>
2021-02-28 07:37:46 +0700

    Refactor: make project only backend

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
+const cors = require('cors');
+app.use(cors({ optionsSuccessStatus: 200 }));
 
-app.use(express.static('public'));
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.send('Backend API app! Read README to use API.');
 });
 
 const convertDate = (date) => {
@@ -38,13 +35,12 @@ const convertDate = (date) => {
       }
     }
   }
-
 }
 
 app.get('/api/timestamp/:date?', (req, res) => {
   res.json(convertDate(req.params.date))
 })
 
-var listener = app.listen(3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+app.listen(3000, function () {
+  console.log('Your app is listening on port 3000');
 });
